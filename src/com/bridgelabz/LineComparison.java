8f9e09b6e4944f923d2e7bc7e49ad2788c3c3770
RajendraGund <rajendragund2015@gmail.com>
2021-09-01 16:17:25 +0530

    Added : Program for Compair the line is greter smaller or same

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -3,12 +3,14 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class LineComparison {
-    public static int x,x1,x2,x3,y,y1,y2,y3;
-    public static double distance1,distance2;
+    public static int x, x1, x2, x3, y, y1, y2, y3;
+    public static double distance1, distance2;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Line comparison program !!");
         input();
         distance();
+        equality();
     }
 
     public static void input() {
@@ -31,12 +33,20 @@ public class LineComparison {
         y3 = sc.nextInt();
     }
 
-        public static void distance()
-        {
-            distance1=Math.sqrt((x1-x)*(x1-x) + (y1-y)*(y1-y));
-            System.out.println("Distance = " +distance1);
-            distance2=Math.sqrt((x3-x2)*(x3-x2) + (y3-y2)*(y3-y2));
-            System.out.println("Distance = " +distance2);
+    public static void distance() {
+        distance1 = Math.sqrt((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y));
+        System.out.println("Distance = " + distance1);
+        distance2 = Math.sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+        System.out.println("Distance = " + distance2);
 
+    }
+
+    public static void equality() {
+        if (distance1 == distance2)
+            System.out.print("Length are same ");
+        else if (distance1 > distance2)
+            System.out.print(" distance1 is greater ");
+        else
+            System.out.println("distance2 is greater");
     }
 }
